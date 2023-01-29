3f4fbbdeaba57efefc605fa6331561389a0384c6
Fabian Haupt <fride@DESKTOP-BKDM.3921>
2023-01-29 05:21:58 +0000

    Api done

---
+++ KeySystemBot/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace KeySystemBot
+{
+    internal class Program
+    {
+		static VMApi API = new VMApi();
+		String Version = "0.1";
+		enum Server
+		{
+			valid_os,
+			Windows_2019_eval,
+			CentOS_7
+		}
+
+		private List<long> TrustedUsers = new List<long>();
+
+		
+		private DiscordSocketClient _client;
+		DiscordConfig DiscordConfig_config;
+		DiscordSocketConfig DiscordSocketConfig_discordSocketConfig = new DiscordSocketConfig();
+		static void Main(string[] args)
+        {
+			
+			new Program().MainAsync().GetAwaiter().GetResult();
+		}
+
+		public async Task MainAsync()
+		{
+			var token = "MTA2OTA3NTQwODI2OTYxMTEyOQ.GcQ9i9.b4ImlKBRazCFnj5jfNtM3lTS0LKmD7i21FDhZw";
+			_client = new DiscordSocketClient();
+			_client.MessageReceived += CommandHandler;
+			_client.Log += Log;
+			DiscordConfig_config = new DiscordConfig();
+			DiscordConfig_config.UseInteractionSnowflakeDate= true;
+			DiscordSocketConfig_discordSocketConfig.LogGatewayIntentWarnings= true;
+			var config = new DiscordSocketConfig
+			{
+				GatewayIntents = GatewayIntents.All
+			};
+			 var client = new DiscordSocketClient(config);
+			client.MessageReceived += CommandHandler;
+			client.Log += Log;
+			await client.LoginAsync(TokenType.Bot, token);
+			await client.StartAsync();
+			
+
+			await Task.Delay(-1);
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(((LogMessage)(msg)).ToString((StringBuilder)null, true, true, DateTimeKind.Local, (int?)11));
+			return Task.CompletedTask;
+		}
+		
+		private async Task<Task> CommandHandler(SocketMessage message)
+		{
+			//1 2 3
+			if (message == null)
+				Console.WriteLine("Message is Null");
+
+			Console.WriteLine("+++LINE: " + message.Content);
+			
+			if (message.Content.StartsWith("!Create"))
+			{
+				String[] Args = message.Content.Split(' ');
+				EmbedBuilder val = new EmbedBuilder();
+				val.AddField("Test", 1, true);
+				val.WithTitle("Create Server");
+				val.WithDescription("Server created Sucessfully ID: " );
+				val.WithColor(Color.Red);
+				await message.Channel.SendMessageAsync("Content: ", false, val.Build());
+				return Task.CompletedTask;
+			}
+
+			
+			
+			if (message.Content == "help")
+			{
+				EmbedBuilder val7 = new EmbedBuilder();
+				val7.WithTitle("Help");
+				val7.WithDescription("- ");
+				val7.WithColor(Color.Teal);
+				await message.Channel.SendMessageAsync("", false, val7.Build(), (RequestOptions)null, (AllowedMentions)null, (MessageReference)null, (MessageComponent)null, (ISticker[])null, (Embed[])null, (MessageFlags)0);
+			}
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ KeySystemBot/VMApi.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Net.Rest;
+using RestSharp;
+
+
+namespace KeySystemBot
+{
+	internal class VMApi
+	{
+
+
+		//Ubuntu 22.10
+		//Debian 11.0
+		enum Server
+		{
+			valid_os,
+			Windows_2019_eval,
+			CentOS_7
+		}
+
+
+		RestClient client;
+		public String List()
+		{
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/list");
+			var request = new RestRequest(Method.GET);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+			return response.Content;
+		}
+		public String GetPrices()
+		{
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/prices");
+			var request = new RestRequest(Method.GET);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+			return response.Content;
+		}
+		public bool ConfigureServer(int Cores,int Ram,int Disk)
+		{
+
+
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/price");
+			var request = new RestRequest(Method.PUT);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			client.AddDefaultParameter("cores", Cores);
+			client.AddDefaultParameter("mem", Ram);
+			client.AddDefaultParameter("disk", Disk);
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+			if (response.Content.Contains("success"))
+				return true;
+			else
+				return false;
+		}
+		public bool ResetPasword(String VMID)
+		{
+
+
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/"+ VMID + "/reset-root");
+			var request = new RestRequest(Method.PUT);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			//client.AddDefaultHeader("cores", "2");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+			if (response.Content.Contains("success"))
+				return true;
+			else
+				return false;
+		}
+		public bool CreateServer(Enum Server)
+		{
+
+
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/create");
+			var request = new RestRequest(Method.PUT);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			client.AddDefaultParameter("cores", "2");
+			client.AddDefaultParameter("mem", "3500");
+			client.AddDefaultParameter("disk", "20");
+			client.AddDefaultParameter("os", Server.ToString().Replace("_", " "));
+			//client.AddDefaultHeader("cores", "2");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+			if (response.Content.Contains("success"))
+				return true;
+			else
+				return false;
+		}
+
+		public bool DeleteServer(String VMID)
+		{
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/" + VMID + "/delete");
+			var request = new RestRequest(Method.PUT);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			//client.AddDefaultHeader("cores", "2");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+
+			if (response.Content.Contains("success"))
+				return true;
+			else
+				return false;
+		}
+
+		public bool ReinstallServer(String VMID, Enum Server)
+		{
+
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/" + VMID + "/reinstall");
+			var request = new RestRequest(Method.PUT);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			client.AddDefaultParameter("os", Server.ToString().Replace("_", " "));
+			//client.AddDefaultHeader("cores", "2");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+
+			if (response.Content.Contains("success"))
+				return true;
+			else
+				return false;
+
+
+		}
+
+		public bool Setrdns(String VMID,String Domain)
+		{
+
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/" + VMID + "/rdns");
+			var request = new RestRequest(Method.POST);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			client.AddDefaultParameter("domain", Domain);
+			//client.AddDefaultHeader("cores", "2");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+
+			if (response.Content.Contains("success"))
+				return true;
+			else
+				return false;
+		}
+
+		public String Status(String VMID)
+		{
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/" + VMID + "/status");
+			var request = new RestRequest(Method.GET);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+			return response.Content;
+		}
+		public enum ServerStatus
+		{
+			start,
+			stop,
+			restart
+		}
+		public bool SetStatus(String VMID,Enum Status)
+		{
+
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/" + VMID + "/status/"+Status);
+			var request = new RestRequest(Method.PUT);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			//client.AddDefaultHeader("cores", "2");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+
+			if (response.Content.Contains("success"))
+				return true;
+			else
+				return false;
+		}
+		public String GetSnapShots(String VMID)
+		{
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/" + VMID + "/snapshot");
+			var request = new RestRequest(Method.GET);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+			return response.Content;
+		}
+
+
+		public bool CreateSnapshot(String VMID)
+		{
+
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/" + VMID + "/snapshot");
+			var request = new RestRequest(Method.PUT);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+			if (response.Content.Contains("success"))
+				return true;
+			else
+				return false;
+		}
+
+		public bool DeleteSnapShot(String VMID,String identifier)
+		{
+
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/" + VMID + "/snapshot/" + identifier);
+			var request = new RestRequest(Method.DELETE);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+			if (response.Content.Contains("success"))
+				return true;
+			else
+				return false;
+		}
+
+		public bool RecoverSnapshot(String VMID, String identifier)
+		{
+
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/" + VMID + "/snapshot/" + identifier);
+			var request = new RestRequest(Method.POST);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+			if (response.Content.Contains("success"))
+				return true;
+			else
+				return false;
+		}
+		public String GetSubscrption()
+		{
+			
+			client = new RestClient("https://sandbox.reselling.24fire.de/accounting/unpaid");
+			var request = new RestRequest(Method.GET);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+			return response.Content;
+		}
+		public String Config(String VMID)
+		{
+
+			client = new RestClient("https://sandbox.reselling.24fire.de/vm/" + VMID + "/config");
+			var request = new RestRequest(Method.GET);
+			client.AddDefaultHeader("Authorization", "1tWXW6lIy7LPKYLJM2HERp4805C3rGZ9");
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+			return response.Content;
+
+			
+		}
+	}
+}
